78f5c87fe5f956d8d40ce22eec1ab04f79ed6f1d
pengfan <fan.peng.nj@gmail.com>
2013-01-18 07:35:03 +0800

    增加数据库基础接口

---
+++ car/android/src/com/gulf/car/data/DataSource.java
@@ -0,0 +1,15 @@
+package com.gulf.car.data;
+
+import java.util.List;
+
+import com.gulf.car.model.MetaRecord;
+
+/*
+ * 数据源接口
+ */
+public interface DataSource {
+
+	void wirteRecord(float money, long kilometers);
+	
+	List<MetaRecord> readRecords();
+}
---
+++ car/android/src/com/gulf/car/model/MetaRecord.java
@@ -0,0 +1,17 @@
+package com.gulf.car.model;
+
+import java.util.Date;
+
+/**
+ * 原始的记录信息
+ * @author fortransit
+ *
+ */
+public class MetaRecord {
+
+	private String id;
+	private long kilometers;
+	private float money;
+	private float fuel;
+	private Date time;
+}
